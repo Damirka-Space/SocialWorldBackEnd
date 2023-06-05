6aecadc17f58c27c913540842c350276f77861d2
Damirka1 <hamitov.damir3101@icloud.com>
2023-06-05 13:35:49 +0600

    Initial commit

---
+++ src/main/java/space/damirka/socialworldbackend/SocialWorldBackEndApplication.java
@@ -0,0 +1,13 @@
+package space.damirka.socialworldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWorldBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialWorldBackEndApplication.class, args);
+    }
+
+}
